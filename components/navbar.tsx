acc453de28df53375792cb6bf52f73836253fc88
Faisal25DEC <faisalmd25121999@gmail.com>
2023-11-24 16:57:51 +0530

    navbar hover completed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ElementRef, RefObject, useRef } from "react";
+import React, { ElementRef, Ref, RefObject, useRef } from "react";
 import {
   FiArrowDown,
   FiInstagram,
@@ -10,6 +10,7 @@ import { FaRegUser, FaTiktok, FaTwitter } from "react-icons/fa";
 import { RiArrowDropDownLine } from "react-icons/ri";
 const Navbar = () => {
   const brandDropdownRef = useRef<ElementRef<"div">>(null);
+  const shopDropdownRef = useRef<ElementRef<"div">>(null);
   const rightNavIcons = [
     FaTwitter,
     FaTiktok,
@@ -42,49 +43,82 @@ const Navbar = () => {
 
   const shopDropDownTwo = ["phone cases", "frame posters", "posters"];
 
+  const handleMouseOver = (ref: RefObject<HTMLDivElement>) => {
+    if (ref && ref.current) {
+      ref.current.style.display = "flex";
+    }
+  };
+  const handleMouseLeave = (ref: RefObject<HTMLDivElement>) => {
+    if (ref && ref.current) {
+      ref.current.style.display = "none";
+    }
+  };
+
   return (
-    <div className=" bg-neutral-800 w-full h-[12vh] m-0 p-0 flex items-center justify-between px-12 fixed">
+    <div className=" bg-neutral-800 w-full h-[12vh] m-0 p-0 flex items-center justify-between px-12 fixed ">
       <div className="flex items-center gap-8 text-white font-semibold text-muted-foreground text-xl">
-        <div className="">
+        <div
+          className="relative py-8"
+          onMouseLeave={() => {
+            handleMouseLeave(brandDropdownRef);
+          }}
+        >
           <p
             onMouseOver={() => {
-              if (brandDropdownRef && brandDropdownRef.current) {
-                brandDropdownRef.current.style.display = "flex";
-              }
+              handleMouseOver(brandDropdownRef);
             }}
-            onMouseLeave={() => {
-              if (brandDropdownRef && brandDropdownRef.current) {
-                brandDropdownRef.current.style.display = "none";
-              }
-            }}
-            className="flex items-center gap-[4] pb-[3px] hover:text-[#f8d878] hover:border-b-[#f8d878] hover:border-b-[1px] cursor-pointer transition duration-500"
+            className="flex items-center gap-[4] pb-[3px] hover:text-[#f8d878] hover:border-b-[#f8d878] hover:border-b-[1px] cursor-pointer transition duration-800"
           >
             brand <RiArrowDropDownLine className="text-[34px]" />
-            <div
-              ref={brandDropdownRef}
-              className="absolute top-[12vh] left-8   hidden flex-col gap-2 w-[max-content] bg-neutral-800 text-white p-8  "
-            >
-              {brandDropdown.map((item, idx) => (
-                <p key={idx} className="hover:text-[#f8d878] cursor-pointer">
-                  {item}{" "}
-                </p>
-              ))}
-            </div>
           </p>
+          <div
+            onMouseLeave={() => {
+              handleMouseLeave(brandDropdownRef);
+            }}
+            ref={brandDropdownRef}
+            className="absolute  hidden flex-col gap-2 w-[max-content] bg-[#323232] text-white p-8 top-[10vh]  "
+          >
+            {brandDropdown.map((item, idx) => (
+              <p
+                key={idx}
+                className="text-white hover:text-[#f8d878] cursor-pointer"
+              >
+                {item}{" "}
+              </p>
+            ))}
+          </div>
         </div>
-        <div className="">
-          <p className=" flex items-center  pb-[3px] hover:text-[#f8d878] hover:border-b-[#f8d878] hover:border-b-[1px] cursor-pointer transition duration-500">
+        <div
+          onMouseLeave={() => {
+            handleMouseLeave(shopDropdownRef);
+          }}
+          className="py-8"
+        >
+          <p
+            onMouseOver={() => {
+              handleMouseOver(shopDropdownRef);
+            }}
+            className=" flex items-center  pb-[3px] hover:text-[#f8d878] hover:border-b-[#f8d878] hover:border-b-[1px] cursor-pointer transition duration-800 "
+          >
             shop <RiArrowDropDownLine className="text-[34px]" />
           </p>
-          <div className="w-[100%] bg-neutral-800 absolute top-[12vh] left-0 hidden">
-            <div className="w-[70%] flex items-start justify-around h-[50vh] pt-[4vh]">
+          <div
+            ref={shopDropdownRef}
+            className="hidden w-full bg-[#323232] absolute top-[12vh]  left-0  text-white "
+          >
+            <div
+              onMouseLeave={() => {
+                handleMouseLeave(shopDropdownRef);
+              }}
+              className="w-[70%] flex items-start justify-around h-[50vh] pt-[4vh]"
+            >
               <div>
                 <h3 className="font-bold text-[25px] py-4">apparel</h3>
 
                 {shopDrowDownOne.map((item, idx) => (
                   <p
                     key={idx}
-                    className="font-medium py-2 hover:text-[#f8d878] cursor-pointer"
+                    className="font-medium py-2 text-white hover:text-[#f8d878] cursor-pointer"
                   >
                     {item}
                   </p>
@@ -95,7 +129,7 @@ const Navbar = () => {
                 {shopDropDownTwo.map((item, idx) => (
                   <p
                     key={idx}
-                    className="py-2 font-medium hover:text-[#f8d878] cursor-pointer"
+                    className="py-2 font-medium text-white hover:text-[#f8d878] cursor-pointer"
                   >
                     {item}
                   </p>
